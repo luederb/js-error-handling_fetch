837d1428612542e24e36aeaa61bd9ffd941d2cf3
Lueder Budde <lueder.budde@gmail.com>
2023-10-11 14:05:32 +0200

    changed try scope

--- js/index.js
+++ js/index.js
@@ -1,23 +1,37 @@
 console.clear();
 
 const userElement = document.querySelector(".user");
-
+const errorMessage = document.querySelector(".error");
 async function loadUser(url) {
   console.log(url);
   const response = await fetch(url);
-  const json = await response.json();
-  const user = json.data;
-
-  userElement.innerHTML = `
+  try {
+    if (!response.ok) {
+      console.log("Network error - couldn't load user ");
+      userElement.innerHTML = "";
+      errorMessage.textContent = "Network error";
+      return null; //ist sowieso der default
+    }
+    const json = await response.json();
+    const user = json.data;
+    console.log("Response: ", json);
+    console.log("User Data: ", user);
+    if (!user) {
+      console.log("user nicht gefunden");
+      return null;
+    }
+    userElement.innerHTML = `
   <h2>${user.first_name} ${user.last_name}</h2>
   <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
   `;
+  } catch (error) {
+    userElement.innerHTML = "";
+    errorMessage.textContent = "Error parsing JSON";
+  }
 }
-
-document
-  .querySelectorAll("button[data-url]")
-  .forEach((button) =>
-    button.addEventListener("click", (event) =>
-      loadUser(event.target.dataset.url)
-    )
-  );
+document.querySelectorAll("button[data-url]").forEach((button) =>
+  button.addEventListener("click", (event) => {
+    loadUser(event.target.dataset.url);
+    console.log("event: ", event);
+  })
+);
